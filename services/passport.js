c64cd571a40b10870c753b51cbcb7dbc7d7cfae7
mindsworth <princegoziem@gmail.com>
2018-05-02 14:23:00 +0100

    --amend

---
+++ services/passport.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const User = require('../models/user');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+    console.log(user);
+  });
+});
+
+passport.use(
+  new googleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id }).save().then(user => {
+            done(null, user);
+          });
+        }
+      });
+    }
+  )
+);
