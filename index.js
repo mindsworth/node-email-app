55b426676fc76a1c73f64e6319fbe7804def954a
mindsworth <princegoziem@gmail.com>
2018-05-02 14:22:32 +0100

    --amend

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./services/passport');
+require('./models/user');
 
-const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(keys.mongoURL)
+  .then(() => {
+    console.log('Database Conneccted!');
+  })
+  .catch(() => {
+    console.log('Unable to connect to the pointed Database!!');
+  });
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ greeting: 'Hello' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`The server is listening at port ${PORT}`);
